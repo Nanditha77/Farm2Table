49407f90c598c5cc6ab05d072733bc1f45303ea7
Nanditha Shetty <nandithaarunshetty@gmail.com>
2025-08-02 17:23:57 +0530

    changed cookie setting for axios

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
